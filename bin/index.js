2d7a8ab2214344ddcb3ba9e054e471b8d505d9c2
Divyansh Tripathi <55395696+theoden42@users.noreply.github.com>
2023-01-22 13:43:27 +0530

    add command line interface

--- bin/index.js
+++ bin/index.js
@@ -3,10 +3,49 @@
 const yargs = require('yargs');
 const chalk = require('chalk');
 const boxen = require('boxen');
+const clear = require('clear');
+const figlet = require('figlet');
+const { getNews } = require('../scripts/newsfetch');
+require('dotenv').config();
 
 
-const introduction = boxen(chalk.bold.blue('Welcome to the NEWSAPI CLI'), {padding: 1, borderStyle: 'double', title: 'HELLO', titleAlignment: 'center', textAlignment: 'center'});
+fetchnews = async(keyword, count) => {
+    const response = await getNews(keyword, count);
+    const articles = response.articles.results;
 
-const usage = chalk.keyword('orange')("\nCommand to execute: newsfind -d <Date to find news> -t1 <category: sports, celebrities, currentaffairs>");
+    articles.forEach(element => {
+        const title = chalk.bold.red(element.title);
+        const body = chalk.bold.white(element.body);
+        const display = boxen(body, {pading: 1, borderStyle: 'double', title: title, titleAlignment: 'center', textAlignment: 'center'});
+        console.log(display);
+    });
+};
 
+
+const introduction = chalk.bold.yellowBright(figlet.textSync('NEWS ON TERMINAL', { horizontalLayout: 'full' }));
+const usage = chalk.bold.keyword('orange')("\nCommand to execute: \nnewsfind -k=<keyword you want to search for> -c=<number of articles you want to read should not exceed 5>");
+
+
+const options = yargs
+.usage(usage)
+.option("k", {alias:"keyword", describe: "keyword to search in article", type: "string", demandOption: true})
+.option("c", {alias:"count", describe: "tell how many article to show for given keyword", type: "number", demandOption: true})
+.help(true)
+.argv;
+
+
+clear();
+const argv = require('yargs/yargs')(process.argv.slice(2)).argv;
 console.log(introduction);
+const k = argv.k;
+const c = argv.c;
+if(c < 5){
+    yargs.help();
+}
+fetchnews(argv.k, argv.c);
+
+
+
+
+
+
